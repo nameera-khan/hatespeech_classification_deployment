0678bcf1115c9e2eacef2eb08fb1caaeb516d247
Nameera Nilofer Khan <82845744+nameera-khan@users.noreply.github.com>
2025-11-02 17:23:45 +0400

    Add files via upload

---
+++ main_hate_speech.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import torch
+import torch.nn as nn
+from transformers import DataCollatorWithPadding,AutoModelForSequenceClassification, Trainer, TrainingArguments,AutoTokenizer,AutoModel,AutoConfig
+import numpy as np
+import pandas as pd
+import time
+
+@st.cache(allow_output_mutation=True)
+def get_model(): 
+  tokenizer = tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-hate-latest")
+  model = AutoModelForSequenceClassification.from_pretrained("decodedplot/hatespeech-roberta")
+  return tokenizer, model
+
+tokenizer,model = get_model()
+
+user_input = st.text_area("Enter Text To Detect Hate")
+button = st.button("Analyse")
+
+d = { 1:'Hate',
+     0:'Not Hate'}
+
+if user_input and button: 
+  test_sample = tokenizer([user_input], padding=True, truncation=True, return_tensors='pt')
+  output = model(**test_sample)
+  st.write("Logits: ", output.logits)
+  y_pred = np.argmax(output.logits.detach().numpy(), axis=1)
+  st.write("Detected: ", d[y_pred[0]])
+
+
